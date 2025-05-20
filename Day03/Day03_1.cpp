fcdef23ea7646c20f2dc6402f816498057aaad29
dergl <167422576+dergl@users.noreply.github.com>
2025-05-20 11:25:54 +0200

    Added function to split one string into a vector of strings, where each of the strings starts with the given delimiter, and only contains it once

--- Day03/Day03_1.cpp
+++ Day03/Day03_1.cpp
@@ -23,14 +23,33 @@ std::vector<std::string> split_string_at_mul( std::string const& str )
 {
     std::vector<std::string> result;
     std::string delimiter{ "mul(" };
+
+    auto first{ str.find( delimiter ) };
+    auto last{ str.find_last_of( delimiter ) - (delimiter.size()-1) };
+
+    while ( first < last )
+    {
+        auto next{ str.find( delimiter, first + 1 ) };
+        result.push_back( str.substr( first, next - first ) );
+        first = next;
+    }
+    result.push_back( str.substr( last, str.size()-1 ) );
+
+    // for ( auto const& s : result )
+    // {
+    //     std::cout << "substring: " << s << std::endl;
+    // }
+    return result;
 }
 
 
 int main() 
 {
-    std::string fileName{ "input.txt" };
+    std::string fileName{ "test_input.txt" };
     std::string input{ input_to_single_string(fileName) };
     
     std::cout << "Input: " << input << std::endl;
+    std::vector<std::string> split_input{ split_string_at_mul(input) };
+    
     return 0;
 }
